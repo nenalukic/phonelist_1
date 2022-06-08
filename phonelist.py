9c9978089a04740d018cc54f9dfe243e75f3f92a
nenalukic <nenamilkov@gmail.com>
2022-06-08 14:03:37 +0200

    #2.6.2 §6. - added remove print

--- phonelist.py
+++ phonelist.py
@@ -48,6 +48,8 @@ while True: ## REPL - Read Execute Program Loop
     elif cmd == "DELETE":
         name = input("  Name: ")
         delete_phone(conn, name)
+    elif cmd == "REMOVE":
+        print("  Unknown command: REMOVE")    
     elif cmd == "QUIT":
         save_phonelist(conn)
         exit()
